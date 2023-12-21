eb45abfdcd246b3a567ae258e23ad531b4609c17
Ondrej Musil <musil.ondrejj@gmail.com>
2023-12-21 21:54:42 +0100

    fix functions.php styling

--- functions.php
+++ functions.php
@@ -1,33 +1,33 @@
 <?php
 
-include_once( get_template_directory() . '/inc/branding.php' );
-include_once( get_template_directory() . '/inc/hardening.php' );
-include_once( get_template_directory() . '/inc/performance.php' );
+include_once(get_template_directory() . '/inc/branding.php');
+include_once(get_template_directory() . '/inc/hardening.php');
+include_once(get_template_directory() . '/inc/performance.php');
 
+function bitspecter_setup()
+{
+    do_action('bitspecter_before_setup');
 
-function bitspecter_setup() {
-    do_action( 'bitspecter_before_setup' );
-
-    $logoDefaults = apply_filters( 'bitspecter_logo_defaults', array(
+    $logoDefaults = apply_filters('bitspecter_logo_defaults', array(
         'height'               => 100,
         'width'                => 400,
         'flex-height'          => true,
         'flex-width'           => true,
-        'header-text'          => array( 'site-title', 'site-description' ),
-        'unlink-homepage-logo' => true, 
+        'header-text'          => array('site-title', 'site-description'),
+        'unlink-homepage-logo' => true,
     ));
 
-    load_theme_textdomain( 'bitspecter', get_template_directory() . '/languages' );
-    add_theme_support( 'automatic-feed-links' );
-    add_theme_support( 'title-tag' );
+    load_theme_textdomain('bitspecter', get_template_directory() . '/languages');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
 
-    add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 150, 150, true ); // Příklad velikosti
+    add_theme_support('post-thumbnails');
+    set_post_thumbnail_size(150, 150, true); // Příklad velikosti
 
-    add_theme_support( 'customize-selective-refresh-widgets' );
-    add_theme_support( 'custom-logo', $logoDefaults );
-    
-    add_theme_support( 'html5', array(
+    add_theme_support('customize-selective-refresh-widgets');
+    add_theme_support('custom-logo', $logoDefaults);
+
+    add_theme_support('html5', array(
         'search-form',
         'comment-form',
         'comment-list',
@@ -35,10 +35,9 @@ function bitspecter_setup() {
         'caption',
     ));
 
-    do_action( 'bitspecter_after_setup' );
+    do_action('bitspecter_after_setup');
 }
 
-add_action( 'after_setup_theme', 'bitspecter_setup' );
-
-add_filter( 'auto_plugin_update_send_email', fn() => false );
-add_filter( 'auto_theme_update_send_email', fn() => false );
+add_action('after_setup_theme', 'bitspecter_setup');
+add_filter('auto_plugin_update_send_email', fn () => false);
+add_filter('auto_theme_update_send_email', fn () => false);
