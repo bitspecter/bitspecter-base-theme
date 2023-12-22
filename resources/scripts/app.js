afa56b95a488fd1e954fb0b78b210943c3f9418b
Ondrej Musil <musil.ondrejj@gmail.com>
2023-12-22 17:02:10 +0100

    Revert "replace bud with laravel-mix for better os compatibility"
    This reverts commit 98ed45c6ad8f77003ec58554759990bf078949b5.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,1 +1,13 @@
-console.log('Hello from BitSpecter! Good luck with your project!');
+import domReady from '@roots/sage/client/dom-ready';
+
+/**
+ * Application entrypoint
+ */
+domReady(async () => {
+  // ...
+});
+
+/**
+ * @see {@link https://webpack.js.org/api/hot-module-replacement/}
+ */
+if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error);
--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('resources/scripts/app.js', 'dist').setPublicPath('public')
-.postCss("resources/styles/app.css", "public/css", [
-  require("tailwindcss"),
-]);
