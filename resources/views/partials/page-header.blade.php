98ed45c6ad8f77003ec58554759990bf078949b5
Ondrej Musil <musil.ondrejj@gmail.com>
2023-12-22 14:19:05 +0100

    replace bud with laravel-mix for better os compatibility

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-  <h1 class="text-3xl font-bold leading-tight text-gray-900 text-blue-500">
+  <h1 class="text-3xl font-bold leading-tight text-blue-500">
     {!! $title !!}
   </h1>
 </div>
---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+let mix = require('laravel-mix');
+
+mix.js('resources/scripts/app.js', 'dist').setPublicPath('public')
+.postCss("resources/styles/app.css", "public/css", [
+  require("tailwindcss"),
+]);
